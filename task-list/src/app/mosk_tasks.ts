30a2825aa4c8f2e5edadd29f1146047f5eb5074e
Lisandro Rios <rios.lisandro369@gmail,com>
2023-01-07 15:07:17 -0300

    task-list v1.3.0

---
+++ task-list/src/app/mosk_tasks.ts
@@ -0,0 +1,21 @@
+import { Task } from "./task";
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text:'terminar porfolio web',
+        day: '11/5/23',
+        reminder: true
+    },
+    {
+        id:2,
+        text:'terminar porfolio web',
+        day: '11/9/23',
+        reminder: true
+    },
+    {
+        id:3,
+        text:'terminar porfolio web',
+        day: '1/12/23',
+        reminder: false
+    }
+]
